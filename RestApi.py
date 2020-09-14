f8f37c954f51ff9878816a1ee1d3a879fc548d20
Malte Niedereichholz <malte@Maltes-Air.fritz.box>
2020-09-14 22:18:46 +0200

    api endpoints

--- RestApi.py
+++ RestApi.py
@@ -1,20 +1,22 @@
+# import required flask extensions
 from flask import Flask
 from flask_restful import reqparse, Api, Resource
+# import Database class
 from Database import Database
 
 db_connection = Database()  # init db connection
 app = Flask(__name__)  # create flask app
-api = Api(app)  # create api for app
-parser = reqparse.RequestParser()  # init request parser
+api = Api(app)  # create api for flask app
 
+parser = reqparse.RequestParser()  # init request parser
 # arguments to parse for POST product
 parser.add_argument('name', type=str, help='product name')
 parser.add_argument('price_threshold', type=float, help='price threshold for notification')
 parser.add_argument('url', type=str, help='geizhals product url')
 
 
-class Products(Resource):  # products endpoint
-    def get(self):  # returns product list
+class ProductList(Resource):  # class for list of products
+    def get(self):
         raw_result = db_connection.execute_query('SELECT name, price_threshold, url FROM products;')
         result_list = []
         for product in raw_result:
@@ -27,38 +29,47 @@ class Products(Resource):  # products endpoint
         result = {'products': result_list}
         return result
 
-    def post(self):  # creates new product in db with params name, price_threshold and url
+
+class CreateProduct(Resource):  # class to add a product
+    def post(self):
         arguments = parser.parse_args()  # parse arguments
         # get single arguments out of dict
         name = arguments['name']
         price_threshold = arguments['price_threshold']
         url = arguments['url']
         Database.insert_product(db_connection, name, price_threshold, url)
-        return f'Inserted {name}, {price_threshold}, {url} into products'
+        return f'Inserted: name: {name} |price_threshold: {price_threshold} |url: {url}'
 
-    def delete(self, product_id):  # delete product from database
+
+class Product(Resource):  # class to delete (and edit product)
+    def get(self, product_id):
+        result = db_connection.execute_query(f'SELECT id, name, price_threshold, url FROM products WHERE id = {product_id}')
+        return result
+
+    def delete(self, product_id):  # delete product
         Database.delete_product(db_connection, product_id)
         return f'Product with id {product_id} deleted'
 
 
-class Prices(Resource):  # prices endpoint to return all prices
+class PriceList(Resource):  # class for price list
     def get(self):
         raw_result = db_connection.execute_query('SELECT product_id, price FROM prices;')
         result = {'prices': raw_result}
         return result
 
 
-class PriceProductId(Resource):  # returns price to a product id
+class Price(Resource):  # class for single price
     def get(self, product_id):
-        raw_result = db_connection.execute_query(f'SELECT price FROM prices WHERE product_id = {product_id}')
-        result = {'data': raw_result}
+        raw_result = db_connection.execute_query(f'SELECT product_id, price FROM prices WHERE product_id = {product_id}')
+        result = {'price': raw_result}
         return result
 
 
-api.add_resource(Products, '/products')  # add products endpoint
-api.add_resource(Prices, '/prices')  # add prices ep
-api.add_resource(PriceProductId, '/prices/<product_id>')  # add price per product_id
+api.add_resource(ProductList, '/productlist')  # add productlist endpoint
+api.add_resource(CreateProduct, '/product/create')  # add create endpoint
+api.add_resource(Product, '/product/<product_id>')  # add product endpoint
+api.add_resource(PriceList, '/prices')  # add pricelist endpoint
+api.add_resource(Price, '/price/<product_id>')  # add price endpoint
 
 if __name__ == '__main__':
     app.run()
-
